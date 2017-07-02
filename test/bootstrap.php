ef881493258d6135eca8b884d40a3b03cb79a697
Florian Gärber <hello@floriangaer.be>
2017-07-02 17:53:52 +0200

    Removed env.php from test
    Will not test actually sending, only everything until it would reach the
    Mailgun API client

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
 
 require dirname(__DIR__).'/vendor/autoload.php';
-
-$env = require dirname(__DIR__).'/test/env.php';
-
-define('MAILGUN_KEY', $env['MAILGUN_KEY']);
-define('MAILGUN_DOMAIN', $env['MAILGUN_DOMAIN']);
